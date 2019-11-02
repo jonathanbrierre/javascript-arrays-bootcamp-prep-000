af4414917bf272f4541693dfdf6090d4da0c9457
jonathan brierre <jonathanbrierre@yahoo.com>
2019-11-02 18:35:37 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,40 @@
+function addElementToBeginningOfArray(array,element) {
+  var newArray = [...array]
+  newArray = newArray.unshift(element)
+  return(newArray)
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element){
+  array = array.unshift(element)
+  return(array)
+}
+
+function addElementToEndOfArray(array,element){
+   var newArray = [...array]
+  newArray = newArray.push(element)
+  return(newArray)
+}
+
+function destructivelyAddElementToEndOfArray(array,element){
+  array = array.push(element)
+  return(array)
+}
+
+function accessElementInArray(array,index){
+  return(array[index])
+  
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return(array)
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return(array.slice(1))
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array = array.pop()
+  return(array)
+}
