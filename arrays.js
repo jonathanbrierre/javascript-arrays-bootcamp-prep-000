458b06ac7e03a4446a312be03b126c26ceefa89e
jonathan brierre <jonathanbrierre@yahoo.com>
2019-11-02 18:58:36 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -39,4 +39,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return(array)
 }
 
-function 
+function removeElementFromEndOfArray(array) {
+  return(array.slice(0,array.length - 1))
+}
